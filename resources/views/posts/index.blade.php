b1e3b992c9fa62609250cf8231be0a654c369405
rdmug1421 <ralmughidi@gmail.com>
2023-03-28 04:47:19 +0300

    finishing functnality

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -20,17 +20,7 @@
       <a href="">{{$post->user->name}}</a> <span>{{$post->created_at->diffForHumans()}}</span>
       <p> {{$post->body}}</p>
     </div>
-<div class="d-flex mb-3">
-  <form action="{{route('posts.likes',$post)}}" method="POST">
-    @csrf 
-    <button type="submit" class="btn btn-outline-success mr-3">Like</button>  
-   </form>
-<form action="" method="post">
-@csrf 
-<button type="submit" class="btn btn-outline-danger">unLike</button>  
-</form>
-<span>{{$post->likes->count()}}{{Str::plural('like', $post->likes->count())}}</span>
-</div>
+
 <hr>
 
     @endforeach
